58b80d9cfb00ccef264287df8cc270e8dce644b9
Анастасия <Angelice92@mail.ru>
2022-04-01 22:25:42 +0300

    feat: add settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,16 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './header/login/login.component';
 import { SearchInputComponent } from './header/search-input/search-input.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
-import { FiltersComponent } from './filters/filters.component';
+import { FiltersComponent} from "./header/filters/filters.component";
 import { SearchItemComponent } from './search-results/search-item/search-item.component';
 import { SearchInfoComponent } from './search-results/search-item/search-info/search-info.component';
-import { InputComponent } from './filters/input/input.component';
-import { ButtonsComponent } from './filters/buttons/buttons.component';
 import { AngularMaterialModule } from "./material/material.module";
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatTreeModule } from "@angular/material/tree";
 
 @NgModule({
   declarations: [
@@ -28,10 +28,10 @@ import {MatInputModule} from "@angular/material/input";
     FiltersComponent,
     SearchItemComponent,
     SearchInfoComponent,
-    InputComponent,
-    ButtonsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AngularMaterialModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AngularMaterialModule,
+    MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatExpansionModule,
+    MatTreeModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
