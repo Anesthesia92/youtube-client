d15144c34dccb787fd1172529fca5184fbd8db90
Анастасия <Angelice92@mail.ru>
2022-04-07 00:52:14 +0300

    feat: add all statistics

--- src/app/search-results/search-info/search-info.component.ts
+++ src/app/search-results/search-info/search-info.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { SearchItemComponent } from "../search-item/search-item.component";
+import { ItemService } from "../../services/item.service";
+import { SearchItem } from "../models/search-response.model";
+import { SearchResults } from "../models/search-item.model";
+
 
 @Component({
   selector: 'app-search-info',
   templateUrl: './search-info.component.html',
   styleUrls: ['./search-info.component.scss'],
 })
-export class SearchInfoComponent {}
+
+export class SearchInfoComponent {
+
+  public cardsList: SearchResults | undefined;
+  public itemCards: SearchItem[] = [];
+  @Input() item: any;
+
+  constructor (private searchItemComponent: SearchItemComponent, private itemService: ItemService) {
+  }
+
+  ngOnInit(): void {
+    this.itemService.filterItemsList$.subscribe(() => {
+      this.cardsList = this.itemService.getAllCards();
+    })
+  this.cardsList = this.itemService.getAllCards();
+    this.itemCards = this.cardsList.items;
+    console.log(this.itemCards)
+  }
+}
--- src/app/search-results/search-item/search-item.component.ts
+++ src/app/search-results/search-item/search-item.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Output} from '@angular/core';
 import { SearchResults } from "../models/search-item.model";
 import { ItemService } from "../../services/item.service";
 import { SearchItem } from "../models/search-response.model";
-
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent implements OnInit {
+export class SearchItemComponent{
 
   public cardsList: SearchResults | undefined;
   public itemCards: SearchItem[] = [];
+
   constructor (private itemService: ItemService) {
   }
 
   ngOnInit(): void {
     this.cardsList= this.itemService.getAllCards();
     this.itemCards = this.cardsList.items;
-    console.log( this.itemCards)
+    console.log(this.itemCards)
   }
+
 }
