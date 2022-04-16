8be8e260c6f83d553d9810abd5087b14a4ce2dfa
Анастасия <Angelice92@mail.ru>
2022-04-17 00:26:11 +0300

    feat: add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from "./core/components/header/header.component";
-import { SearchResultsComponent } from "./youtube/pages/search-results/search-results.component";
 import {PageAuthComponent} from "./auth/pages/page-auth/page-auth.component";
+import {AuthGuard} from "./auth/guards/auth.guard";
+import {NotFoundComponent} from "./auth/components/not-found/not-found.component";
 
 const routes: Routes = [
-  { path: '', component: PageAuthComponent },
-  { path: 'start', component: HeaderComponent },
-  { path: 'main', component: SearchResultsComponent },
-];
+  {
+    path: '',
+    component: PageAuthComponent,
+    children: [
+      // {
+      //   path: '', redirectTo: '/login', pathMatch: 'full'
+      // },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('../app/auth/components/authorization/authorization.component').then(
+            (a) => a.AuthorizationComponent)
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('../app/youtube/pages/search-results/search-results.component').then(
+            (m) => m.SearchResultsComponent),
+            canLoad: [AuthGuard],
+      },
+      {
+        path: '404', component: NotFoundComponent
+      },
+      {
+        path: '**', redirectTo: '404'
+      },
+    ]
+  }
+]
 
 @NgModule({
   imports: [
