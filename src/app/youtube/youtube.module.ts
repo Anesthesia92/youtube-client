8f5bd8f49ad5c8edcd077606f8c4d8e32e19057b
Анастасия <Angelice92@mail.ru>
2022-04-26 00:13:43 +0300

    feat: add logout button

---
+++ src/app/youtube/youtube.module.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { SearchResultsComponent } from "./pages/search-results/search-results.component";
+import { SearchItemComponent} from "./components/search-item/search-item.component";
+import { SearchInfoComponent} from "./components/search-info/search-info.component";
+import { MatIconModule} from "@angular/material/icon";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatFormFieldModule} from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatCardModule } from "@angular/material/card";
+import { FormsModule } from "@angular/forms";
+import { YoutubeRoutingModule } from "./youtube-routing.module";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { BottomColorDirective } from "../shared/directives/bottom-color.directive";
+import { SortingPipe } from "./pipes/sorting.pipe";
+import { MatButtonModule } from "@angular/material/button";
+
+@NgModule({
+  declarations: [SearchItemComponent, SearchInfoComponent, SearchResultsComponent, BottomColorDirective,
+    SortingPipe],
+  imports: [CommonModule, YoutubeRoutingModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule,
+    MatExpansionModule, MatTreeModule, MatCardModule, FormsModule, YoutubeRoutingModule, MatButtonModule],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [SearchResultsComponent]
+})
+
+export class YoutubeModule { }
